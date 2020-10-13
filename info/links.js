968f1979288175e5efacd0c236e951045e796c3a
Panagiotis Vamvakas <57200277+Vuxxs@users.noreply.github.com>
2020-10-13 13:43:02 +0300

    Create links.js

---
+++ info/links.js
@@ -0,0 +1,5 @@
+"Gramweb: http://gram-web.ionio.gr/unistudent/",
+"Webmail: https://webmail.ionio.gr/",
+"DII: http://di.ionio.gr/en/",
+"Ionio: https://ionio.gr/",
+"Eudoxos: https://service.eudoxus.gr/student/"
